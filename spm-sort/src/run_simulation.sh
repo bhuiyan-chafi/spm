c64619971544e1a790e80f3b6e3c59d10883a84d
ASM CHAFIULLAH <chafiullah@outlook.com>
2025-11-16 22:50:13 +0100

    removed 48 workers

--- spm-sort/src/run_simulation.sh
+++ spm-sort/src/run_simulation.sh
@@ -1,4 +1,5 @@
 # run: bash run_simulation.sh > /dev/null 2>&1 &
+# srun --nodes=1 --ntasks=1 --cpus-per-task=32 --time=00:30:00 bash run_simulation.sh 32 > /dev/null 2>&1 &
 # Be very careful about this script, don't be stupid unless you are confident about your program
 # if you are using srun then book it for proper time lap
 # otherwise you are occupying resources for nothing
@@ -63,7 +64,7 @@ echo -e "Starting Sequential implementation:\n" >> logs/run_$ts.txt 2>&1
 }
 echo -e "Starting OpenMP implementation:\n" >> logs/run_$ts.txt 2>&1
 {
-    for WORKERS in 2 4 8 16 32 48;do
+    for WORKERS in 2 4 8 16 32;do
         ./openmp 1M 256 $WORKERS $MEMORY_CAP >> logs/run_$ts.txt 2>&1
         ./verify ../data/rec_1M_256.bin >> logs/run_$ts.txt 2>&1
 
@@ -89,7 +90,7 @@ echo -e "Starting OpenMP implementation:\n" >> logs/run_$ts.txt 2>&1
 
 echo -e "Starting FastFlow FARM implementation:\n" >> logs/run_$ts.txt 2>&1
 {
-    for WORKERS in 2 4 8 16 32 48;do
+    for WORKERS in 2 4 8 16 32;do
         ./farm 1M 256 $WORKERS $MEMORY_CAP >> logs/run_$ts.txt 2>&1
         ./verify ../data/rec_1M_256.bin >> logs/run_$ts.txt 2>&1
 
