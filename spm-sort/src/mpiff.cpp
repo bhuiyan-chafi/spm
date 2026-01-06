c6602525eae40716bf18e375243740ba7ce84856
ASM CHAFIULLAH <chafiullah@outlook.com>
2026-01-06 05:06:03 +0100

    finalized

--- spm-sort/src/mpiff.cpp
+++ spm-sort/src/mpiff.cpp
@@ -742,7 +742,9 @@ int main(int argc, char **argv)
 
     int world_rank = 0;
     int world_size = 0;
+    // return the ID of the process as rank
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // determines the number of processes in the communicator
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     try
